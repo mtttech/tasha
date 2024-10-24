4732ba71d4b4afc637ae57cf9055eeb8a982be98
Marcus T Taylor <mtaylor9754@hotmail.com>
2024-10-24 13:29:30 -0400

    Code clean up.

--- tasha/tasha.py
+++ tasha/tasha.py
@@ -1,6 +1,6 @@
 from dataclasses import asdict, replace
 from pathlib import Path
-from typing import Dict, List, Literal, Union
+from typing import Dict, List
 
 from rich.console import Console
 from rich.progress import track
