c3267cc8a986d0bc077a46f27a87d5e35e2d4189
Marcus T Taylor <mtaylor9754@hotmail.com>
2024-12-12 10:13:59 -0500

    Changed comment for class method purpose.

--- src/tasha/settings.py
+++ src/tasha/settings.py
@@ -6,7 +6,7 @@ import toml
 class SettingsLoader:
 
     def __init__(self) -> None:
-        """Loads the settings file."""
+        """Handles the loading of tasha's settings file."""
         settings_path = Path.home() / ".config" / "tasha" / "settings" / "default.toml"
         if settings_path.exists():
             with settings_path.open() as loaded_settings:
