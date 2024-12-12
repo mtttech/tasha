a7af04971cbade9d355cc1c28303f04fb22b348c
Marcus T Taylor <mtaylor9754@hotmail.com>
2024-12-12 10:15:20 -0500

    Code clean up.

--- src/tasha/themes.py
+++ src/tasha/themes.py
@@ -7,19 +7,22 @@ import toml
 class ThemeLoader:
 
     def __init__(self, theme_file: str | None = None) -> None:
-        """Loads the requested theme file.
+        """Handles the loading of tasha's themes.
 
         Args:
             theme_file (str): Theme file to load."""
         theme_dir = Path.home() / ".config" / "tasha" / "themes"
-        self.current_theme_path = theme_dir / "default.toml"
+        if not theme_dir.exists():
+            raise FileNotFoundError("Cannot locate tasha's theme directory.")
 
         if theme_file is not None:
-            check_theme_path = theme_dir / f"{theme_file}.toml"
-            if check_theme_path.exists():
-                self.current_theme_path = check_theme_path
+            theme_path = theme_dir / f"{theme_file}.toml"
+            if theme_path.exists():
+                self.current_theme_path = theme_path
+        else:
+            self.current_theme_path = theme_dir / "default.toml"
 
-    def loadTheme(self) -> Dict[str, str]:
+    def load(self) -> Dict[str, str]:
         """Loads the set theme file.
 
         Returns:
