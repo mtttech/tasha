688aba5eaf0f913aabb236fdde8a2c437288537e
Marcus T Taylor <mtaylor9754@hotmail.com>
2025-05-29 08:18:28 -0400

    First commit.

---
+++ src/tasha/css.py
@@ -0,0 +1,7 @@
+stylesheet = {
+    "default": "bold green",
+    "exit": "bold dim red",
+    "menu.index": "bold italic cyan",
+    "menu.option": "bold magenta",
+    "prompt": "bold dim green",
+}
