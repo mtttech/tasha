e18ed6be5d3375d500542c2fecde2a1b87b80bd0
mtttech <mtaylor9754@hotmail.com>
2025-06-01 16:04:58 -0400

    Added more info to PlayerCharacter test case.

--- tests/test_actor.py
+++ tests/test_actor.py
@@ -3,6 +3,14 @@ from tasha.actor import PlayerCharacter
 
 oPC = PlayerCharacter(
     alignment="Chaotic Good",
+    attributes={
+        "Strength": {"score": 15, "modifier": 2},
+        "Dexterity": {"score": 12, "modifier": 1},
+        "Constitution": {"score": 13, "modifier": 1},
+        "Intelligence": {"score": 10, "modifier": 0},
+        "Wisdom": {"score": 10, "modifier": 0},
+        "Charisma": {"score": 8, "modifier": -1},
+    },
     gender="Male",
     name="Woldor the Barbarian",
     species="Human",
