28cdccd6df3a24c7c9184d34615b659e7678eefd
Marcus T Taylor <mtaylor9754@hotmail.com>
2025-09-24 00:37:17 -0400

    Drop old python code.

--- tests/test_tasha.py
+++
@@ -1,9 +0,0 @@
-from tasha.tasha import assign_ability_scores, calculate_modifier
-
-
-def test_assign_ability_scores():
-    assert type(assign_ability_scores()) == dict
-
-
-def test_calculate_modifier():
-    assert calculate_modifier(18) == 4
