6bcb50af58da9420eae0d979500100ef7c15f82a
Marcus T Taylor <mtaylor9754@hotmail.com>
2025-10-15 12:42:58 -0400

    Merged redundant functions.

--- utils/utils.go
+++ utils/utils.go
@@ -4,30 +4,17 @@ Copyright © 2025 Marcus Taylor <mtaylor9754@hotmail.com>
 package utils
 
 /*
-Returns the given items sequence minus the first instance of needle.
+Returns the given items minus the first instance of needle.
 */
-func OmitInt(items []int, needle int) []int {
-	newIntArray := []int{}
+func OmitItem[T comparable](items []T, needle T) []T {
+	newSlice := []T{}
 	needleFound := false
 	for _, item := range items {
 		if !needleFound && needle == item {
 			needleFound = true
 		} else {
-			newIntArray = append(newIntArray, item)
+			newSlice = append(newSlice, item)
 		}
 	}
-	return newIntArray
-}
-
-/*
-Returns the given items sequence minus the instance of needle.
-*/
-func OmitStr(items []string, needle string) []string {
-	newStrArray := []string{}
-	for _, item := range items {
-		if needle != item {
-			newStrArray = append(newStrArray, item)
-		}
-	}
-	return newStrArray
+	return newSlice
 }
