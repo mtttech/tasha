139f06a59823da478a956c8917b3bab0d194b216
Marcus T Taylor <mtaylor9754@hotmail.com>
2025-10-17 10:16:06 -0400

    Renamed module abilities from attributes.

--- actor/actor.go
+++ actor/actor.go
@@ -4,18 +4,18 @@ Copyright © 2025 Marcus Taylor <mtaylor9754@hotmail.com>
 package actor
 
 import (
-	"tasha/attributes"
+	"tasha/abilities"
 	"tasha/d20"
 )
 
 type Config struct {
 	PlayerCharacter struct {
-		Name          string                             `toml:"name"`
-		Species       string                             `toml:"species"`
-		Gender        string                             `toml:"gender"`
-		Background    string                             `toml:"background"`
-		Classes       map[string]d20.Class               `toml:"classes"`
-		AbilityScores map[string]attributes.AbilityScore `toml:"ability_score"`
-		Skills        []string                           `toml:"skills"`
+		Name          string                            `toml:"name"`
+		Species       string                            `toml:"species"`
+		Gender        string                            `toml:"gender"`
+		Background    string                            `toml:"background"`
+		Classes       map[string]d20.Class              `toml:"classes"`
+		AbilityScores map[string]abilities.AbilityScore `toml:"ability_score"`
+		Skills        []string                          `toml:"skills"`
 	}
 }
