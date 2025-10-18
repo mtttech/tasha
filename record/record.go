bf2b8409b907b730338b92afc7838780922768f2
Marcus T Taylor <mtaylor9754@hotmail.com>
2025-10-18 19:13:22 -0400

    Renamed to record from actor.

---
+++ record/record.go
@@ -0,0 +1,21 @@
+/*
+Copyright © 2025 Marcus Taylor <mtaylor9754@hotmail.com>
+*/
+package record
+
+import (
+	"tasha/abilities"
+	"tasha/d20"
+)
+
+type CharacterSheetTOMLSchema struct {
+	PC struct {
+		Name          string                            `toml:"name"`
+		Species       string                            `toml:"species"`
+		Gender        string                            `toml:"gender"`
+		Background    string                            `toml:"background"`
+		Classes       map[string]d20.Class              `toml:"classes"`
+		AbilityScores map[string]abilities.AbilityScore `toml:"ability_score"`
+		Skills        []string                          `toml:"skills"`
+	}
+}
