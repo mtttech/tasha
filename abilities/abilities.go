5c0d8363bf7c8ae9b08c4e062ce2ed334c615886
Marcus T Taylor <mtaylor9754@hotmail.com>
2025-10-21 02:44:44 -0400

    Code clean up.

--- abilities/abilities.go
+++ abilities/abilities.go
@@ -17,40 +17,40 @@ type AbilityScore struct {
 Drops the lowest value, then adds the remaining three values.
 */
 func add_three() int {
-	results := roll_four()
-	smallest_int := slices.Min(results)
-	smallest_int_found := false
-	total := 0
-	for _, n := range results {
-		if n == smallest_int && !smallest_int_found {
-			smallest_int_found = true
+	rollFourResults := roll_four()
+	smallestInt := slices.Min(rollFourResults)
+	isSmallestIntFound := false
+	totalValue := 0
+	for _, n := range rollFourResults {
+		if n == smallestInt && !isSmallestIntFound {
+			isSmallestIntFound = true
 			continue
 		}
-		total += n
+		totalValue += n
 	}
-	return total
+	return totalValue
 }
 
 /*
 Returns four numbers between 1 and 6.
 */
 func roll_four() []int {
-	var rolls []int
+	var rolledValues []int
 	for i := 1; i < 5; i++ {
-		rolls = append(rolls, rand.Intn(6)+1)
+		rolledValues = append(rolledValues, rand.Intn(6)+1)
 	}
-	return rolls
+	return rolledValues
 }
 
 /*
 Returns six results of add_three().
 */
 func roll_six() []int {
-	var results []int
+	var rolledValues []int
 	for i := 1; i < 7; i++ {
-		results = append(results, add_three())
+		rolledValues = append(rolledValues, add_three())
 	}
-	return results
+	return rolledValues
 }
 
 /*
@@ -71,16 +71,16 @@ Will automatically reroll the results if one of the following is true:
  2. If the largest attribute < 15
 */
 func GenerateScores() []int {
-	var dice_rolls []int
+	var generatedScores []int
 	for {
-		dice_rolls = roll_six()
-		if slices.Min(dice_rolls) >= 8 && slices.Max(dice_rolls) >= 15 {
+		generatedScores = roll_six()
+		if slices.Min(generatedScores) >= 8 && slices.Max(generatedScores) >= 15 {
 			break
 		}
 	}
-	slices.Sort(dice_rolls)
-	slices.Reverse(dice_rolls)
-	return dice_rolls
+	slices.Sort(generatedScores)
+	slices.Reverse(generatedScores)
+	return generatedScores
 }
 
 /*
